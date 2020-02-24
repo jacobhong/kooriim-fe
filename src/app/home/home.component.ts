a9ed33ca7aba091eae63840055ecd85e0dd7c4cc
jacob.hong <jacob.hong@gmail.com>
2020-02-23 20:26:03 -0800

    add home page background

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'home-background',
+      sanitizer.bypassSecurityTrustResourceUrl('/assets/home-background.png'));
+  }
   ngOnInit() {
   }
 
