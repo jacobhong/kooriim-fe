6083b5b73cbcc89c49977346f311821d31c0b232
jacob.hong <jacob.hong@gmail.com>
2020-08-15 10:18:24 -0700

    update urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,13 +3,13 @@ export const environment = {
         allowOrigin: 'https://now.kooriim.com',
         albums: 'photo-album-service/albums',
         assets: 'https://now.kooriim.com/',
-        auth: 'https://keycloak.kooriim.com:8443',
-        fileUpload: 'https://keycloak.kooriim.com:8080/photo-album-service/photos',
-        baseUrl: 'https://keycloak.kooriim.com:8080',
+        auth: 'https://keycloak.kooriim.com/keycloak',
+        fileUpload: 'https://keycloak.kooriim.com/photo-album-service/photos',
+        baseUrl: 'https://keycloak.kooriim.com',
         baseContext: '',
         photo: 'photo-album-service/photos',
         login: 'photo-album-service/login',
-        keycloak: 'https://keycloak.kooriim.com:8443/auth',
+        keycloak: 'https://keycloak.kooriim.com/keycloak/auth',
         home: 'https://now.kooriim.com/home'
     },
     production: true
