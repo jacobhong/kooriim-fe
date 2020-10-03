db416a988c3aebc7e8d29eb2314c870ecc992534
jacob.hong <jacob.hong@gmail.com>
2020-10-02 17:04:41 -0700

    refactor ui

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material';
 import { DomSanitizer } from '@angular/platform-browser';
+import { KeycloakService } from '../shared/keycloak/keycloak.service';
 
 @Component({
   selector: 'app-home',
@@ -8,14 +9,11 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-    iconRegistry.addSvgIcon(
-      'home-background',
-      sanitizer.bypassSecurityTrustResourceUrl('/assets/home-background.png'));
-  }
+  userName: string;
+  constructor(private keycloak: KeycloakService) {}
 
   ngOnInit() {
+    this.userName = (this.keycloak.keycloak.tokenParsed as any).name;
   }
 
 }
