6b673055ca77a04fee8a9cd275107d59ae448dd0
jacob.hong <jacob.hong@gmail.com>
2021-02-05 16:24:47 -0800

    update keycloak url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,13 +3,13 @@ export const environment = {
         allowOrigin: 'https://now.kooriim.com',
         albums: 'photo-album-service/albums',
         assets: 'https://now.kooriim.com/',
-        auth: 'https://login.kooriim.com',
+        auth: 'https://keycloak.kooriim.com:8443',
         fileUpload: 'https://photos.kooriim.com/photo-album-service/photos',
         baseUrl: 'https://photos.kooriim.com',
         baseContext: '',
         photo: 'photo-album-service/photos',
         login: 'photo-album-service/login',
-        keycloak: 'https://login.kooriim.com/auth',
+        keycloak: 'https://keycloak.kooriim.com:8443/auth',
         home: 'https://now.kooriim.com/home'
     },
     production: true
