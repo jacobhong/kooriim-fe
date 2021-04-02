5d53ad5267ae40b8aaba9c0709364ec8475f6bff
jacob.hong <jacob.hong@gmail.com>
2021-04-01 21:30:14 -0700

    add google sync to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     routes: {
-        allowOrigin: 'https://now.kooriim.com',
         albums: 'photo-album-service/albums',
+        allowOrigin: 'https://now.kooriim.com',
         assets: 'https://now.kooriim.com/',
         auth: 'https://login.kooriim.com',
         fileUpload: 'https://photos.kooriim.com/photo-album-service/photos',
@@ -9,9 +9,10 @@ export const environment = {
         baseContext: '',
         photo: 'photo-album-service/photos',
         login: 'photo-album-service/login',
+        googleSync: 'photo-album-service/google-sync',
+        video: 'photo-album-service/videos',
         keycloak: 'https://login.kooriim.com/auth',
-        home: 'https://now.kooriim.com/home',
-        googleSync: 'photo-album-service/google-sync'
+        home: 'https://now.kooriim.com/home,
     },
     production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     routes: {
+        albums: 'photo-album-service/albums',
         allowOrigin: 'http://192.168.1.206.xip.io:4200',
         assets: 'http://192.168.1.206.xip.io:4200/',
         auth: 'https://192.168.1.206.xip.io:8443',
-        albums: 'photo-album-service/albums',
         fileUpload: 'http://192.168.1.206.xip.io:4200/photo-album-service/photos',
         baseUrl: 'http://192.168.1.206.xip.io:4200',
         baseContext: '',
