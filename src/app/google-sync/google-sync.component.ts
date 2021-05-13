e94d7f2875a94aa3d47428d1bd2f68d75d94a135
jacob.hong <jacob.hong@gmail.com>
2021-05-12 19:51:20 -0700

    test

--- src/app/google-sync/google-sync.component.ts
+++ src/app/google-sync/google-sync.component.ts
@@ -9,37 +9,40 @@ import { PhotoServiceComponent } from '../photo/photo-service/photo-service.comp
   styleUrls: ['./google-sync.component.scss']
 })
 export class GoogleSyncComponent implements OnInit {
-  // range = new FormGroup({
-  //   start: new FormControl(),
-  //   end: new FormControl()
-  // });
-  // startDate: number;
-  // endDate: number;
+  range = new FormGroup({
+    start: new FormControl(),
+    end: new FormControl()
+  });
+  startDate: number;
+  endDate: number;
 
-  // startMinDate: Date;
-  // startMaxDate: Date;
-  // endMinDate: Date;
-  // endMaxDate: Date; 
+  startMinDate: Date;
+  startMaxDate: Date;
+  endMinDate: Date;
+  endMaxDate: Date; 
   constructor(private photoService: PhotoServiceComponent) {
+    // const currentYear = new Date().getFullYear();
+    // this.minDate = new Date(currentYear - 20, 0, 1);
+    // this.maxDate = new Date(currentYear + 1, 11, 31);
    }
 
   ngOnInit() {
   }
 
-  // startDateInput(event: MatDatepickerInputEvent<Date>) {
-  //   this.endMinDate = new Date(event.value.getFullYear(), event.value.getMonth() + 3, event.value.getDate());
-  //   this.endMaxDate = new Date(event.value.getFullYear(), event.value.getMonth() + 3, event.value.getDate());
-  //   this.startDate = event.value.valueOf();
-  // }
+  startDateInput(event: MatDatepickerInputEvent<Date>) {
+    this.endMinDate = new Date(event.value.getFullYear(), event.value.getMonth() + 3, event.value.getDate());
+    this.endMaxDate = new Date(event.value.getFullYear(), event.value.getMonth() + 3, event.value.getDate());
+    this.startDate = event.value.valueOf();
+  }
 
-  // endDateInput(event: MatDatepickerInputEvent<Date>) {
-  //   this.endDate = event.value.valueOf();
-  // }
+  endDateInput(event: MatDatepickerInputEvent<Date>) {
+    this.endDate = event.value.valueOf();
+  }
 
-  // googleSync() {
-  //   this.photoService.googleSync(this.startDate, this.endDate).subscribe(result => {
-  //     console.log('finished sgoogle sync');
-  //   });
-  // }
+  googleSync() {
+    this.photoService.googleSync(this.startDate, this.endDate).subscribe(result => {
+      console.log('finished sgoogle sync');
+    });
+  }
 
 }
